ee9d8bbb36a3c94ff02381b47a14f104f59a6ec2
nicho <nicho@FLAZERS>
2023-02-07 21:10:05 +0400

    Добавьте файлы проекта.

---
+++ HimApp/Controllers/UIObj.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HimApp.Controllers
+{
+    internal class UIObj
+    {
+        public static void UpdateColor(String path)
+        {
+            var uri = new Uri(@path, UriKind.Relative);
+            ResourceDictionary Dictionary = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(Dictionary);
+        }
+    }
+}
