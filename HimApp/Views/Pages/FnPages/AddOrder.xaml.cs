1f8788cc671ac02f66eb798aa2aa3cc7a763ebaa
Flazers <nicholas620@mail.ru>
2023-03-09 16:22:28 +0400

    upd

--- HimApp/Views/Pages/FnPages/AddOrder.xaml.cs
+++ HimApp/Views/Pages/FnPages/AddOrder.xaml.cs
@@ -3,6 +3,7 @@ using HimApp.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,6 +21,13 @@ namespace HimApp.Views.Pages.FnPages
     /// <summary>
     /// Логика взаимодействия для AddOrder.xaml
     /// </summary>
+    
+    public class OrderComplit
+    {
+        //client
+        public static Client client;
+    }
+
     public partial class AddOrder : Page
     {
         int PageSetIndex = 0;
@@ -35,6 +43,7 @@ namespace HimApp.Views.Pages.FnPages
             ShowPage();
             executor.ItemsSource = HimBDEntities.GetContext().Users.ToList();
             category_car.ItemsSource = HimBDEntities.GetContext().CarBody.ToList();
+            ClientList.ItemsSource = HimBDEntities.GetContext().Client.ToList();
             //condition.ItemsSource = HimBDEntities.GetContext().Conditions.ToList();
         }
 
@@ -72,6 +81,104 @@ namespace HimApp.Views.Pages.FnPages
                     OrderSetPageOne.Visibility = Visibility.Visible;
                     break;
             }
+        }
+
+        private void addClientForm_Click(object sender, RoutedEventArgs e)
+        {
+            HideOtherPage();
+            TitleOther.Text = "Выбор клиента";
+            ClientPageOther.Visibility = Visibility.Visible;
+        }
+
+        private void addAutoForm_Click(object sender, RoutedEventArgs e)
+        {
+            HideOtherPage();
+            TitleOther.Text = "Выбор автомобиля";
+            AutoPageOther.Visibility = Visibility.Visible;
+        }
+
+        private void HideOtherPage()
+        {
+            DopPageOther.Visibility = Visibility.Collapsed;
+            ClientPageOther.Visibility = Visibility.Collapsed;
+            AutoPageOther.Visibility = Visibility.Collapsed;
+        }
+
+        private void AddClient_Click(object sender, RoutedEventArgs e)
+        {
+            if(phone_client.Text.Length < 11)
+            {
+                MainVoid.ErrorMessage("Заполните номер телефона");
+                return;
+            }
+            try
+            {
+                if (HimBDEntities.GetContext().Client.Where(x => x.phone.Trim() == phone_client.Text.Trim()).FirstOrDefault() != null)
+                {
+                    MainVoid.InformationMessage("Клиент существует");
+                    return;
+                }
+                HimBDEntities.GetContext().Client.Add(new Client()
+                {
+                    first_name = first_name_client.Text.Trim(),
+                    last_name = last_name_client.Text.Trim(),
+                    phone = phone_client.Text.Trim(),
+                });
+                HimBDEntities.GetContext().SaveChanges();
+                MainVoid.InformationMessage($"Клиент {phone_client.Text.Trim()} добавлен.");
+            }
+            catch (Exception ex)
+            {
+                MainVoid.FatalErrorMessage(ex.ToString());
+            }
+        }
+
+        private void phone_client_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!(Char.IsDigit(e.Text, 0) || (e.Text == ".")
+            && !phone_client.Text.Contains(".")
+            && phone_client.Text.Length != 0))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void SelectClient_Click(object sender, RoutedEventArgs e)
+        {
+            Button thisbt = (Button)sender;
+            int ltp = Convert.ToInt32(thisbt.Tag.ToString());
+            OrderComplit.client = HimBDEntities.GetContext().Client.Where(x => x.id == ltp).FirstOrDefault();
+            info_Client.Text = OrderComplit.client.first_name + " " + OrderComplit.client.last_name;
+            info_ClientNumber.Text = OrderComplit.client.phone;
+        }
+
+        private void AddCar_Click(object sender, RoutedEventArgs e)
+        {
+            //if (phone_client.Text.Length < 11)
+            //{
+            //    MainVoid.ErrorMessage("Заполните номер телефона");
+            //    return;
+            //}
+            //try
+            //{
+            //    if (HimBDEntities.GetContext().Client.Where(x => x.phone.Trim() == phone_client.Text.Trim()).FirstOrDefault() != null)
+            //    {
+            //        MainVoid.InformationMessage("Клиент существует");
+            //        return;
+            //    }
+            //    HimBDEntities.GetContext().Client.Add(new Client()
+            //    {
+            //        first_name = first_name_client.Text.Trim(),
+            //        last_name = last_name_client.Text.Trim(),
+            //        phone = phone_client.Text.Trim(),
+            //    });
+            //    HimBDEntities.GetContext().SaveChanges();
+            //    MainVoid.InformationMessage($"Клиент {phone_client.Text.Trim()} добавлен.");
+            //}
+            //catch (Exception ex)
+            //{
+            //    MainVoid.FatalErrorMessage(ex.ToString());
+            //}
         }
     }
 }
